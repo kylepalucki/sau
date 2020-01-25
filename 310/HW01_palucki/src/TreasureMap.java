2dbf25de16f8c5a69851d359274c0bf5892be75a
Kyle Palucki <paluckikylee@sau.edu>
2020-01-25 14:12:42 -0600

    -

--- 310/HW01_palucki/src/TreasureMap.java
+++ 310/HW01_palucki/src/TreasureMap.java
@@ -74,43 +74,11 @@ public class TreasureMap {
                 f += String.format("%6s", data[i][j] + " |");
             }
             f+="\n";   
-            if (i<data[i].length-1){
+            if (i<8){
                 f+= ("   +-----+-----+-----+-----+-----+-----+-----+-----+-----+\n");
             }
         }
         f+= ("   +-----------------------------------------------------+");
         return f;
     }
-    
-    private static int[][] createRandomBoard() {
-        int[][] board = new int[9][9];
-        int counter=0;
-        for (int rows = 0; rows < 9; rows++) {
-            for (int cols = 0; cols < 9; cols++) {
-                board[rows][cols] = gen();
-                counter++;
-            }
-        }
-        return board;
-    }
-    private static int gen() {
-        Random random = new Random();
-        int num = random.nextInt(100);
-        num = num > 9 ? num : num+10;
-        int coin = random.nextInt(2);
-        return coin == 0 ? num : num*-1;
-    }
-
-    private static void display(int[][] data) {
-        String s = "";
-        for (int r=0;r<data.length;r++) {
-            for (int c=0; c<data[r].length; c++) {
-                s += String.format("%4d", data[r][c]);
-            }
-            s += "\n";
-        }
-        System.out.println(s);
-    }
-    
-    
 }

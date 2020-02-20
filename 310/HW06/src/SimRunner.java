db6a7a4d418d62c89264e1d458c9f69344c5eb46
Kyle♥ <47790150+kylepalucki@users.noreply.github.com>
2020-02-20 14:30:12 -0600

    -

--- 310/HW06/src/SimRunner.java
+++ 310/HW06/src/SimRunner.java
@@ -3,8 +3,8 @@ public class SimRunner {
 
     public static void main(String[] args) {
         CarWashSimulator cws = new CarWashSimulator(20,4,0.09);
-        cws.runSimulation(1234);
-        //cws.runSimulation();
+        //cws.runSimulation(1234);
+        cws.runSimulation();
         System.out.println(cws.getLog());
 
     }
